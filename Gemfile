14327d23972e5bd84ca45f3c029d9633c0e61a5b
Jared Grippe <tech_ops+jared@change.org>
2012-09-17 22:50:56 -0700

    WIP

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in hobson-cli.gemspec
 gemspec
+
+
+group :development do
+  gem 'debugger'
+end
--- lib/hobson/cli.rb
+++ lib/hobson/cli.rb
@@ -1,9 +1,52 @@
-require "hobson/cli/version"
+require "rest_client"
+require "yaml"
+require "irb"
+require "json"
+
+Signal.trap("INT") { puts; exit(1) }
 
 module Hobson
-  module Cli
+  class Cli
 
+    def initialize
+      response = server['test_runs'].post(test_run: test_run_data)
+      p JSON.parse(response)
+    end
 
-      raise "hello"
+    def test_run_data
+      {
+        'project_origin' => project_origin,
+        'sha'            => sha,
+        'requestor'      => requestor,
+      }
+    end
+
+    def server
+      @server ||= RestClient::Resource.new(config[:server])
+    end
+
+    def config
+      @config ||= YAML.load_file('config/hobson.yml')
+    end
+
+    def project_origin
+      origin = `git config --get remote.origin.url`.chomp
+      raise "unable to infer project origin" if origin.nil? || origin.empty?
+      origin
+    end
+
+    def sha
+      `git rev-parse HEAD`.chomp or raise
+    end
+
+    def requestor
+      `git var -l | grep GIT_AUTHOR_IDENT`.split('=').last.split(' <').first
+    rescue
+      raise "unable to infer requestor"
+    end
+
   end
 end
+
+
+Hobson::Cli.new
